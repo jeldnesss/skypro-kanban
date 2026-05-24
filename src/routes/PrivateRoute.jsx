a0931a0afaf954121a0e0e001777d146b6d21741
julia <juliaarlovskaya@mail.ru>
2026-05-24 23:08:18 +0300

    не пропускает на главную без авторизации

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 import AuthContext from '../context/AuthContext';
 
 function PrivateRoute() {
-  const user = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   return user ? <Outlet /> : <Navigate to="/sign-in" replace />;
 }
 
